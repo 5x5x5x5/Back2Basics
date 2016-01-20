d21a3f4414866611192d017628f1c448a71913bb
Danny <daniel.siegle@gmail.com>
2016-01-19 22:53:07 -0500

    a spoonful of scala

--- atomicScala/66.scala
+++ atomicScala/66.scala
@@ -7,3 +7,13 @@ val c = 7
 val check = (a >= b) && (b >= c)
 
 println(check)
+
+// part the second
+
+val doubleCheck = {
+	if(a >= c && b >= c) {
+		println("Both are!")
+	} else {
+		println("one is and one isn't!")
+	}
+}
