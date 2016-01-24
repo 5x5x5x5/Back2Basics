ac5c36ae6d0d83ddfdaf681cbbb8fbd4a117c0f7
Danny <daniel.siegle@gmail.com>
2016-01-23 21:11:06 -0500

    Summary1

---
+++ atomicScala/summary1.scala
@@ -0,0 +1,38 @@
+// Summary1.scala
+
+// name:type = initialization
+
+val name = initialization
+
+val daysInFebruary = 28
+val daysInMarch = 31
+
+var = name = initialization
+var name:type = initialization
+
+var hoursSpent = 20
+hoursSpent = 25
+
+hoursSpent = 30.5 // causes error. Can't change type.
+
+// A statement changes state
+// An expression expresses
+
+val hours = 10
+val minutesPerHour = 60
+val minutes = hours * minutesPerHour
+
+// some expression (like println()) don't seem to produce a result. 
+// Scala has a special Unit type for these
+
+val result = println("???")
+// ???
+// result: Unit = ()
+
+// Conditional expressions
+if (99 < 100) { 4 } else { 42 }
+// res0: Int = 4
+
+val result = if (99 < 100) { Bob } else { Meemee}
+// result: Int = 4
+
