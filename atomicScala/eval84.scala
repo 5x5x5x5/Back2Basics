69b8b18e9a445f21e2bde3efc95e280d20ece50b
Danny <daniel.siegle@gmail.com>
2016-02-03 10:54:01 -0500

    working on classes

--- atomicScala/eval84.scala
+++ atomicScala/eval84.scala
@@ -1,3 +1,5 @@
+val r1 = Range(0, 10)
+
 // eval84.scala
 
 val r = Range(0, 20)
@@ -8,3 +10,14 @@ r2.step
 // 2.
 var s = "This is an experiment"
 s.split(" ")
+
+// 3. 
+
+var s1:String = "Sally"
+var s2:String = "Sally"
+
+if(s1.equals(s2)) println("s1 and s2 are equal")
+else println("s1 and s2 don't seem to muatch up")
+
+// 4. 
+
