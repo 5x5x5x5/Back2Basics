a20b8ff8c2cde2a9bd6da4759119ed56911e6b7f
Danny <daniel.siegle@gmail.com>
2016-02-25 21:59:28 -0500

    first branch practice

---
+++ listicles.py
@@ -0,0 +1,15 @@
+# listicles.py
+
+spam = [2, 3, 5, 3.14, 1, -7]
+spam.sort()
+print(spam)
+
+cram = ['ants', 'zebra', 'quail', 'badgers']
+cram.sort()
+print(cram)
+
+spam.sort(reverse=True)
+cram.sort(reverse=True)
+
+print(spam)
+print(cram)
