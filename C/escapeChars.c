0897e0ae8c1f754c502c2f2f4ad520d5818802fa
Danny <daniel.siegle@gmail.com>
2016-07-30 18:28:13 -0400

    C is warming up

---
+++ C/escapeChars.c
@@ -0,0 +1,16 @@
+// Absolute Beginner's Guide To C, 3rd ed.
+// Chapter 4 Example 1--escapeChars.c
+
+#include <stdio.h>
+
+int main(void)
+{
+	/* These lines show how to use the most popular
+	Escape Sequences*/
+	printf("Column A\tColumn B\tColumn C");
+	printf("\nMy Computer\'s Beep doesn\'t work: \a!\n");
+	printf("\"Letz\bs fix that typo and then show the backslash ");
+	printf("character \\\" she said\n");
+
+	return 0;
+}
