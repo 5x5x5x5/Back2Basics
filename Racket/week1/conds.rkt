4ad3fddedf1c1d318152a2f5c3dc5f3c3240e116
Daniel Siegle <daniel.siegle@gmail.com>
2017-01-22 13:20:35 -0500

    finished module1

---
+++ Racket/week1/conds.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname conds) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
+(define I1 (rectangle 10 20 "solid" "red"))
+(define I2 (rectangle 20 30 "solid" "black"))
+
+(if (< (image-width I1)
+       (image-width I2))
+    "tall"
+    "wide")
+  
